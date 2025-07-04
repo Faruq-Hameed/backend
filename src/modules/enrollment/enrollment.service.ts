f50483d555a5a3a4f31b800e2c0de0bba3bcc0c7
Faruq Hameed <108727947+Faruq-Hameed@users.noreply.github.com>
2025-07-04 21:29:15 +0100

    added enrollment service and flow

--- src/modules/enrollment/enrollment.service.ts
+++ src/modules/enrollment/enrollment.service.ts
@@ -56,7 +56,7 @@ export class EnrollmentService {
       approved: () => {
         enrollment.status = 'approved';
         enrollment.approvedAt = new Date();
-        enrollment.approvedBy = { id: adminId } as any ;
+        enrollment.approvedBy = { id: adminId } as User ;
       },
       rejected: () => {
         enrollment.status = 'rejected';
